c0ecf2c1b0105b7be849400b62a4f3b285a538c6
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2025-04-05 19:01:09 +0700

    Add Toaster component for notifications in the URL shortener page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import GeneralWrapper from "@/components/GeneralWrapper";
 import UrlShortenerForm from "@/components/UrlShortenerForm";
+import { Toaster } from "react-hot-toast";
 
 export default function Home() {
 	return (
 		<GeneralWrapper>
+			<Toaster />
 			<div className="mx-auto w-full max-w-3xl">
 				<h1 className="mb-6 text-center font-bold text-2xl md:text-3xl">
 					URL Shortener
