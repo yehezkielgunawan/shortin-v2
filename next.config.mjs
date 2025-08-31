4d7bca927482ff270b28b9219c1c9e748888899b
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2025-08-31 17:47:16 +0700

    Add Cloudflare env variables to Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    CLOUDFLARE_API_TOKEN: process.env.CLOUDFLARE_API_TOKEN,
+    CLOUDFLARE_ACCOUNT_ID: process.env.CLOUDFLARE_ACCOUNT_ID,
+  },
+};
 
 export default nextConfig;
